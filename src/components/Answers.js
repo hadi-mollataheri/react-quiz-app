d1cb4f9a173f5ad0416943c504b462d3ed21600f
Hadi <mhadimollataheri@gmail.com>
2024-04-28 16:46:22 +0330

    Modify the way ansert buttons was created -Use .map for creating buttons and Use a single event handler with help of the .map, for more readable and manageable code

--- src/components/Answers.js
+++ src/components/Answers.js
@@ -5,69 +5,34 @@ function Answers(props) {
     return prevQuestion + 1;
   };
 
-  const handleOptionOne = (e) => {
-    if (props.answersArray[0].isCorrect === true) {
-      props.setQuestionIndex((prevQuestionIndex) => {
-        return incrementQuestionIndex(prevQuestionIndex);
-      });
-    } else return;
-  };
-
-  const handleOptionTwo = (e) => {
-    if (props.answersArray[1].isCorrect === true) {
-      props.setQuestionIndex((prevQuestionIndex) => {
-        return incrementQuestionIndex(prevQuestionIndex);
-      });
-    } else return;
-  };
-
-  const handleOptionThree = (e) => {
-    if (props.answersArray[2].isCorrect === true) {
-      props.setQuestionIndex((prevQuestionIndex) => {
-        return incrementQuestionIndex(prevQuestionIndex);
-      });
-    } else return;
-  };
-
-  const handleOptionFour = (e) => {
-    if (props.answersArray[3].isCorrect === true) {
-      props.setQuestionIndex((prevQuestionIndex) => {
-        return incrementQuestionIndex(prevQuestionIndex);
-      });
-    } else return;
-  };
-
   return (
     <div
       id='answer-container'
       className='w-full flex flex-col justify-between text-start'
     >
-      <button
-        onClick={handleOptionOne}
-        className='rounded-lg border-sky-900 border-4 text-start pl-1 py-1'
-      >
-        {props.answersArray[0].answerText}
-      </button>
-      <button
-        onClick={handleOptionTwo}
-        className='rounded-lg border-sky-900 border-4 text-start pl-1  py-1'
-      >
-        {props.answersArray[1].answerText}
-      </button>
-      <button
-        onClick={handleOptionThree}
-        className='rounded-lg border-sky-900 border-4 text-start pl-1  py-1'
-      >
-        {props.answersArray[2].answerText}
-      </button>
-      <button
-        onClick={handleOptionFour}
-        className='rounded-lg border-sky-900 border-4 text-start pl-1  py-1'
-      >
-        {props.answersArray[3].answerText}
-      </button>
+      {props.answersArray.map((answerObject, index) => {
+        return (
+          <button
+            onClick={() => {
+              if (answerObject.isCorrect === true) {
+                props.setQuestionIndex((prevQuestionIndex) => {
+                  return incrementQuestionIndex(prevQuestionIndex);
+                });
+              }
+            }}
+            className='rounded-lg border-4 text-start pl-1  py-1'
+            key={index}
+          >
+            {answerObject.answerText}
+          </button>
+        );
+      })}
     </div>
   );
 }
 
 export default Answers;
+
+// TODO: I must use .map for creating buttons this way i can
+// manage the correct and wrong answer better too beside a cleaner
+// and more manageable code
